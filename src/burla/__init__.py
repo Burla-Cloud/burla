d91bde60a501b5c8ea76b3055059b5d238770f9d
Jake Zuliani <jkzuliani40@gmail.com>
2024-07-29 18:51:08 -0400

    fixed global vars

--- src/burla/__init__.py
+++ src/burla/__init__.py
@@ -2,16 +2,16 @@ import os
 from fire import Fire
 
 # Imported here to avoid cyclic imports
-if os.getenv("IN_DEV") == "True":
-    _BURLA_SERVICE_URL = "http://127.0.0.1:5001"
-    # _BURLA_SERVICE_URL = "https://cluster.test.burla.dev"
-    # _BURLA_BACKEND_URL = "http://127.0.0.1:5002"
-    _BURLA_BACKEND_URL = "https://backend.test.burla.dev"
-else:
-    _BURLA_SERVICE_URL = "https://cluster.burla.dev"
-    _BURLA_BACKEND_URL = "https://backend.burla.dev"
+# _BURLA_SERVICE_URL = "http://127.0.0.1:5001"
+# _BURLA_BACKEND_URL = "http://127.0.0.1:5002"
 
-__version__ = "0.1.0-alpha"
+# _BURLA_SERVICE_URL = "https://cluster.test.burla.dev"
+# _BURLA_BACKEND_URL = "https://backend.test.burla.dev"
+
+_BURLA_SERVICE_URL = "https://cluster.burla.dev"
+_BURLA_BACKEND_URL = "https://backend.burla.dev"
+
+__version__ = "0.1.1-alpha"
 
 from burla._auth import login, login_cmd as _login_cmd
 from burla._nas import cd, ls, upload, download

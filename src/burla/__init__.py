8c0c2726edb2c6a8f52253c78d6e4b9b1ee22510
Jake Zuliani <jkzuliani40@gmail.com>
2024-09-03 19:35:55 -0400

    fixes constants, version bump

--- src/burla/__init__.py
+++ src/burla/__init__.py
@@ -1,26 +1,26 @@
 from fire import Fire
 
 # Imported here to avoid cyclic imports
-_BURLA_SERVICE_URL = "http://127.0.0.1:5001"
+# _BURLA_SERVICE_URL = "http://127.0.0.1:5001"
 # _BURLA_BACKEND_URL = "http://127.0.0.1:5002"
 
 # _BURLA_SERVICE_URL = "https://cluster.test.burla.dev"
-_BURLA_BACKEND_URL = "https://backend.test.burla.dev"
+# _BURLA_BACKEND_URL = "https://backend.test.burla.dev"
 
-# _BURLA_SERVICE_URL = "https://cluster.burla.dev"
-# _BURLA_BACKEND_URL = "https://backend.burla.dev"
+_BURLA_SERVICE_URL = "https://cluster.burla.dev"
+_BURLA_BACKEND_URL = "https://backend.burla.dev"
 
-BURLA_JOBS_BUCKET = "burla-jobs"
-# BURLA_JOBS_BUCKET = "burla-jobs-prod"
+# BURLA_JOBS_BUCKET = "burla-jobs"
+BURLA_JOBS_BUCKET = "burla-jobs-prod"
 
-BURLA_GCP_PROJECT = "burla-test"
-# BURLA_GCP_PROJECT = "burla-prod"
+# BURLA_GCP_PROJECT = "burla-test"
+BURLA_GCP_PROJECT = "burla-prod"
 
 INPUTS_TOPIC_PATH = f"projects/{BURLA_GCP_PROJECT}/topics/burla_job_inputs"
 OUTPUTS_SUBSCRIPTION_PATH = f"projects/{BURLA_GCP_PROJECT}/subscriptions/burla_job_outputs"
 LOGS_SUBSCRIPTION_PATH = f"projects/{BURLA_GCP_PROJECT}/subscriptions/burla_job_logs"
 
-__version__ = "0.1.2-alpha.1"
+__version__ = "0.1.2-alpha.2"
 
 from burla._auth import login, login_cmd as _login_cmd
 from burla._remote_parallel_map import remote_parallel_map

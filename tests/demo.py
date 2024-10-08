98413970f86019923e018df59b84a9222cd2618f
Jacob Zuliani <jkzuliani40@gmail.com>
2024-10-08 11:04:24 -0400

    0.8.4 (#10)
    * verison bump
    
    * signifigant refactoring, remove all non-functional functionality
    
    * small interface change
    
    * stuff

--- tests/demo.py
+++ tests/demo.py
@@ -1,14 +1,36 @@
 from burla import remote_parallel_map
+from time import time
 
 
 def my_function(my_input):
-    print(f"hi #{my_input}")
-    return my_input * 2
+
+    called_at = time()
+    return my_input
 
 
-inputs = list(range(8))
+inputs = list(range(1000))
 
-results = remote_parallel_map(my_function, inputs)
+start = time()
+# print(f"STARTED AT: {start}")
+result_generator = remote_parallel_map(my_function, inputs, spinner=False)
 
+# print("[")
+results = []
+# result_received_at = []
+for result in result_generator:
+    # result_received_at.append(time())
+    # print(f"{time()},")
+    results.append(result)
+
+# print("]")
+
+# print("result_received_at times:")
+# print(result_received_at)
+print("--------------------------------------------------")
+print(f"Done after {time() - start}s")
 print(f"num results: {len(results)}")
+# print("results: (document claimed at times)")
 print(results)
+
+unprocessed_inputs = [i for i in inputs if i not in results]
+print(f"{len(unprocessed_inputs)} unprocessed inputs: {unprocessed_inputs}")
--- tests/test_rpm.py
+++ tests/test_rpm.py
@@ -1,3 +1,5 @@
+from time import time
+
 from burla import remote_parallel_map
 from utils import with_packages
 
@@ -39,5 +41,11 @@ def test_base():
         print(my_input)
         return my_input
 
-    something = remote_parallel_map(my_function, list(range(2)))
-    print(something)
+    my_inputs = list(range(4))
+    start = time()
+
+    generator = remote_parallel_map(my_function, my_inputs)
+    results = list(generator)
+
+    print(f"Done after {time()-start}s")
+    print(results)

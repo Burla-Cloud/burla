cf2896c7b5e39486b8ac95583a7843105509eaf2
Jake Zuliani <jkzuliani40@gmail.com>
2024-10-17 19:51:46 -0400

    i am in hell

--- src/burla/_remote_parallel_map.py
+++ src/burla/_remote_parallel_map.py
@@ -1,54 +1,39 @@
-import os
+import io
 import sys
 import requests
-import warnings
 import pickle
 import json
 from six import reraise
 from threading import Thread, Event
 from typing import Callable, Optional
-from time import sleep, time
+from time import sleep
 from queue import Queue
 from uuid import uuid4
+from requests import Response
 
 import cloudpickle
-from yaspin import yaspin, Spinner
+from yaspin import yaspin
 from tblib import Traceback
 from google.cloud import firestore
 
-from burla import _BURLA_SERVICE_URL, __version__, _BURLA_JOBS_BUCKET, _BURLA_GCP_PROJECT
-from burla._env_inspection import get_pip_packages, get_function_dependencies
-from burla._auth import (
-    get_auth_headers,
-    get_gcs_credentials,
-    AuthException,
-)
+from burla import _BURLA_SERVICE_URL, __version__, _BURLA_GCP_PROJECT
+from burla._auth import get_auth_headers, get_gcs_credentials, AuthException
 from burla._helpers import (
-    nopath_warning,
-    JobTimeoutError,
-    InstallError,
-    ServerError,
-    StatusMessage,
     upload_inputs,
     print_logs_from_db,
-    enqueue_outputs_from_db,
+    enqueue_results_from_db,
+    periodiocally_healthcheck_job,
 )
 
-warnings.formatwarning = nopath_warning
-
-
-FUNCTION_SIZE_GCS_THRESHOLD = 25 * 1024 * 1024
-
-MAX_CPUS = 1000
-MAX_GPUS = 200
+# increase at your own risk, burla may break.
 MAX_PARALLELISM = 1000
 
-TIMEOUT_MIN = 60 * 12  # max time a Burla job can run for
-IN_COLAB = os.getenv("COLAB_RELEASE_TAG") is not None
+# this can be kinda high because healthcheck failures should theoretically be pretty rare.
+JOB_HEALTHCHECK_FREQUENCY_SEC = 20
 
 # Try to instantiate clients now to minimize latency when calling `remote_parallel_map`.
-# This will not work for a variety of reasons, (user not logged in yet / planning to use api_key).
-# When it does, this step can be skipped inside `remote_parallel_map`, lowering e2e latency.
+# This may not work for a variety of reasons, (user not logged in yet / planning to use api_key).
+# If it does, this step can be skipped inside `remote_parallel_map`, lowering e2e latency.
 try:
     BURLA_AUTH_HEADERS = get_auth_headers()
     credentials = get_gcs_credentials(BURLA_AUTH_HEADERS)
@@ -58,47 +43,18 @@ except:
     BURLA_AUTH_HEADERS = None
 
 
-# Error from inside the main service that should be passed to the user:
+class UnknownClusterError(Exception):
+    def __init__(self):
+        msg = "An unknown error occurred inside your Burla cluster, "
+        msg += "this is not an error with your code."
+        super().__init__(msg)
+
+
 class MainServiceError(Exception):
-    pass
-
-
-def job_status_poll_rate(seconds_since_job_started: int):
-    """
-    User wants a response quickly if their job is short (check status often early).
-    User wants main_service to not get spammed and be slow (check status less often if job is long).
-    """
-    if seconds_since_job_started < 10:
-        return 0
-    elif seconds_since_job_started < 30:
-        return 0.5
-    elif seconds_since_job_started < 120:
-        return 1.5
-    elif seconds_since_job_started > 120:
-        return 3
-
-
-def raise_any_errors_from_job(job_id: str, headers: dict):
-    """Raises: ServerError, InstallError, HTTPError, or re-raised error from UDF"""
-
-    response = requests.get(f"{_BURLA_SERVICE_URL}/v1/jobs/{job_id}", headers=headers)
-    if response.status_code == 401:
-        AuthException()
-    elif response.status_code >= 500:
-        raise ServerError()
-    elif response.status_code >= 300:
-        response.raise_for_status()
-
-    job = response.json() or {}
-    if job.get("udf_error"):
-        exception_info = pickle.loads(bytes.fromhex(job.get("udf_error")))
-        reraise(
-            tp=exception_info["exception_type"],
-            value=exception_info["exception"],
-            tb=Traceback.from_dict(exception_info["traceback_dict"]).as_traceback(),
-        )
-    elif job.get("install_error"):
-        raise InstallError(job["install_error"])
+    # Error from inside the main_service that should be passed back to the user.
+    def __init__(self, response: Response):
+        self.__class__.__name__ = response.json().get("error_type")
+        super().__init__(response.json().get("message"))
 
 
 class InputsTooBig(Exception):
@@ -108,25 +64,10 @@ class InputsTooBig(Exception):
 def _start_job(
     function_: Callable,
     inputs: list,
-    verbose: bool,
-    spinner: Spinner,
     func_cpu: int,
     func_ram: int,
-    func_gpu: int,
-    parallelism: int,
-    image: Optional[str] = None,
-    packages: Optional[list[str]] = None,
+    max_parallelism: int,
 ) -> str:
-    if verbose:
-        spinner.text = StatusMessage.preparing()
-        spinner.start()
-
-    if packages:
-        required_packages = [{"name": pkg_name} for pkg_name in packages]
-    else:
-        installed_packages = list(get_pip_packages())
-        imported_modules = list(get_function_dependencies(function_))
-        required_packages = [pkg for pkg in installed_packages if pkg["name"] in imported_modules]
 
     inputs_pkl = [cloudpickle.dumps(input_) for input_ in inputs]
     inputs_size = sum([len(input_pkl) for input_pkl in inputs_pkl])
@@ -135,127 +76,101 @@ def _start_job(
 
     # in separate thread start uploading inputs:
     inputs_id = str(uuid4())
-<<<<<<< Updated upstream
-    input_uploader_thread = Thread(
-        target=upload_inputs,
-        args=(DB, inputs_id, inputs),
-        daemon=True,
-    )
-=======
     args = (DB, inputs_id, inputs_pkl)
     input_uploader_thread = Thread(target=upload_inputs, args=args, daemon=True)
->>>>>>> Stashed changes
     input_uploader_thread.start()
 
-    function_pkl = cloudpickle.dumps(function_)
     payload = {
         "n_inputs": len(inputs),
         "inputs_id": inputs_id,
-        "image": image,
         "func_cpu": func_cpu,
         "func_ram": func_ram,
-        "func_gpu": func_gpu,
-        "parallelism": parallelism,
-        "python_version": f"3.10" if sys.version_info.minor < 10 else f"3.{sys.version_info.minor}",
-        "packages": None if image else required_packages,
+        "max_parallelism": max_parallelism,
+        "python_version": f"3.{sys.version_info.minor}",
         "burla_version": __version__,
     }
-    request_size = len(function_pkl) + len(cloudpickle.dumps(payload))
-    send_function_through_gcs = request_size > FUNCTION_SIZE_GCS_THRESHOLD
 
     # tell service to start job
     url = f"{_BURLA_SERVICE_URL}/v1/jobs/"
-    if send_function_through_gcs:
-        response = requests.post(url, json=payload, headers=BURLA_AUTH_HEADERS)
-    else:
-        files = {"function_pkl": function_pkl}
-        data = dict(request_json=json.dumps(payload))
-        response = requests.post(url, files=files, data=data, headers=BURLA_AUTH_HEADERS)
+    files = {"function_pkl": cloudpickle.dumps(function_)}
+    data = dict(request_json=json.dumps(payload))
+    response = requests.post(url, files=files, data=data, headers=BURLA_AUTH_HEADERS)
+    response_is_json = response.headers.get("Content-Type") == "application/json"
 
     if response.status_code == 401:
         raise AuthException()
-    elif response.status_code != 200 and response.json().get("error_type"):
-        msg = f"{response.json().get('error_type')}\n{response.json().get('message')}"
-        raise MainServiceError(msg)
+    elif response.status_code != 200 and response_is_json and response.json().get("error_type"):
+        raise MainServiceError(response)
     else:
         response.raise_for_status()
-        job_id = response.json()["job_id"]
-
-    if send_function_through_gcs:
-        spinner.text = StatusMessage.uploading_function
-        function_blob_name = f"{job_id}/function.pkl"
-        gcs_base_url = "https://www.googleapis.com/upload/storage"
-        function_blob_url_args = f"uploadType=media&name={function_blob_name}"
-        function_blob_url = f"{gcs_base_url}/v1/b/{_BURLA_JOBS_BUCKET}/o?{function_blob_url_args}"
-        bytes_header = {"Content-Type": "application/octet-stream"}
-        requests.post(function_blob_url, headers=bytes_header, data=function_pkl)
-
-    spinner.text = StatusMessage.running()
-    return job_id, input_uploader_thread
+        return response.json()["job_id"]
 
 
-def _watch_job(
-    job_id: str,
-    n_inputs: int,
-    verbose: bool,
-    spinner: Spinner,
-):
+def _watch_job(job_id: str, n_inputs: int, log_msg_stdout: io.TextIOWrapper):
     stop_event = Event()
     job_doc_ref = DB.collection("jobs").document(job_id)
 
-    # Start collecting logs generated by this job using a separate thread.
-    args = (job_doc_ref, stop_event, spinner)
+    # Start printing logs generated by this job from a separate thread.
+    args = (job_doc_ref, stop_event, log_msg_stdout)
     log_thread = Thread(target=print_logs_from_db, args=args, daemon=True)
     log_thread.start()
 
-    # Start collecting outputs generated by this job using a separate thread.
-    output_queue = Queue()
-    args = (job_doc_ref, stop_event, output_queue)
-    output_thread = Thread(target=enqueue_outputs_from_db, args=args, daemon=True)
-    output_thread.start()
+    # Start enqueueing results (either return-values or errors) generated by this job
+    # from a separate thread.
+    result_queue = Queue()
+    args = (job_doc_ref, stop_event, result_queue)
+    result_thread = Thread(target=enqueue_results_from_db, args=args, daemon=True)
+    result_thread.start()
 
-    if verbose:
-        spinner.text = StatusMessage.running()
+    # Run periodic healthchecks on the job/cluster from a separate thread.
+    cluster_error_event = Event()
+    auth_error_event = Event()
+    args = (job_id, JOB_HEALTHCHECK_FREQUENCY_SEC, BURLA_AUTH_HEADERS, stop_event)
+    args += (cluster_error_event, auth_error_event)
+    healthchecker_thread = Thread(target=periodiocally_healthcheck_job, args=args, daemon=True)
+    healthchecker_thread.start()
 
-    start = time()
-    n_outputs_received = 0
-    while n_outputs_received < n_inputs:
-        timed_out = (time() - start) > (TIMEOUT_MIN * 60)
-        sleep(job_status_poll_rate(seconds_since_job_started=time() - start))
+    n_results_received = 0
+    while n_results_received < n_inputs:
+        sleep(0.05)
 
-        raise_any_errors_from_job(job_id, BURLA_AUTH_HEADERS)
+        if cluster_error_event.is_set():
+            raise UnknownClusterError()
+        if auth_error_event.is_set():
+            raise AuthException()
 
-        while not output_queue.empty():
-            n_outputs_received += 1
-            yield output_queue.get()
+        while not result_queue.empty():
+            n_results_received += 1
+            input_index, is_error, result_pkl = result_queue.get()
 
-        if timed_out:
-            raise JobTimeoutError(job_id=job_id, timeout=TIMEOUT_MIN)
+            if is_error:
+                exc_info = pickle.loads(result_pkl)
+                traceback = Traceback.from_dict(exc_info["traceback_dict"]).as_traceback()
+                reraise(tp=exc_info["type"], value=exc_info["exception"], tb=traceback)
+            else:
+                yield cloudpickle.loads(result_pkl)
 
     stop_event.set()
-    log_thread.join()
-    output_thread.join()
 
 
 def remote_parallel_map(
     function_: Callable,
     inputs: list,
-    verbose: bool = True,
-    image: Optional[str] = None,
     func_cpu: int = 1,
-<<<<<<< Updated upstream
-    func_ram: int = 1,
-    func_gpu: int = 0,
-    parallelism: Optional[int] = None,
-=======
     func_ram: int = 4,
     spinner: bool = True,
     max_parallelism: Optional[int] = None,
->>>>>>> Stashed changes
     api_key: Optional[str] = None,
-    packages: Optional[list[str]] = None,
 ):
-    rpm_call_time = time()
+    """
+    TODO:
+    """
+    max_parallelism = max_parallelism if max_parallelism else len(inputs)
+    max_parallelism = max_parallelism if max_parallelism < MAX_PARALLELISM else MAX_PARALLELISM
+    if spinner:
+        spinner = yaspin()
+        spinner.text = f"Preparing to run {len(inputs)} inputs through `{function_.__name__}`"
+        spinner.start()
 
     global DB, BURLA_AUTH_HEADERS
     if (DB is None) or (BURLA_AUTH_HEADERS is None):
@@ -263,43 +178,6 @@ def remote_parallel_map(
         credentials = get_gcs_credentials(BURLA_AUTH_HEADERS)
         DB = firestore.Client(credentials=credentials, project=_BURLA_GCP_PROJECT)
 
-<<<<<<< Updated upstream
-    n_inputs = len(inputs)
-    if (func_cpu > 96) or (func_cpu < 1):
-        raise ValueError("CPU per function call must be one of [1.. 80]")
-    if (func_ram > 624) or (func_ram < 1):
-        raise ValueError("RAM per function call must be one of [1.. 320]")
-    if (func_gpu > 4) or (func_gpu < 0):
-        raise ValueError("GPU per function call must be one of [0.. 4]")
-
-    parallelism = parallelism if parallelism else n_inputs
-    parallelism = parallelism if parallelism < MAX_PARALLELISM else MAX_PARALLELISM
-    requested_cpu = parallelism * func_cpu
-    requested_gpu = parallelism * func_gpu
-
-    if requested_cpu > MAX_CPUS and not (requested_gpu > MAX_GPUS):
-        parallelism = MAX_CPUS // func_cpu
-        msg = f"Limiting parallelism to {parallelism} to stay under limit of {MAX_CPUS} CPUs."
-        warnings.warn(msg)
-    if requested_gpu > MAX_GPUS:
-        parallelism = MAX_GPUS // func_gpu
-        msg = f"Limiting parallelism to {parallelism} to stay under limit of {MAX_GPUS} GPUs."
-        warnings.warn(msg)
-
-    spinner = yaspin()
-    StatusMessage.function_name = function_.__name__
-    StatusMessage.n_inputs = len(inputs)
-    StatusMessage.total_cpus = parallelism * func_cpu
-    StatusMessage.total_gpus = parallelism if func_gpu else 0
-
-    try:
-        job_id = None
-        job_id, input_uploader_thread = _start_job(
-            function_=function_,
-            inputs=inputs,
-            verbose=verbose,
-            spinner=spinner,
-=======
     # wrap user function with a for loop because sending too many inputs causes firestore issues
     # this is a temporary fix:
     max_inputs = 256
@@ -322,19 +200,10 @@ def remote_parallel_map(
         job_id = _start_job(
             function_=function_wrapped,
             inputs=input_batches,
->>>>>>> Stashed changes
             func_cpu=func_cpu,
             func_ram=func_ram,
-            func_gpu=func_gpu,
-            parallelism=parallelism,
-            image=image,
-            packages=packages,
+            max_parallelism=max_parallelism,
         )
-<<<<<<< Updated upstream
-        output_generator = _watch_job(job_id, len(inputs), verbose, spinner)
-        return_values = list(output_generator)
-        input_uploader_thread.join()
-=======
         if spinner:
             spinner.text = f"Running {len(inputs)} inputs through `{function_.__name__}`"
         log_msg_stdout = spinner if spinner else sys.stdout
@@ -342,18 +211,11 @@ def remote_parallel_map(
         for output_batch in _watch_job(job_id, len(input_batches), log_msg_stdout):
             yield from output_batch
 
->>>>>>> Stashed changes
     except Exception as e:
-        spinner.stop()
+        if spinner:
+            spinner.stop()
         raise e
-    finally:
-        if job_id:
-            payload = {"rpm_call_time": rpm_call_time, "job_ended_ts": time()}
-            url = f"{_BURLA_SERVICE_URL}/v1/jobs/{job_id}/ended"
-            requests.post(url, json=payload, headers=BURLA_AUTH_HEADERS)
 
-    if verbose:
+    if spinner:
         spinner.text = "Done!"
         spinner.ok("✔")
-
-    return return_values

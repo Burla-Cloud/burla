31bd2daff424bc6c2ffebfe8248ec7ec57c7365d
Jake Zuliani <jkzuliani40@gmail.com>
2024-10-17 19:46:20 -0400

    stash merge

--- tests/demo.py
+++ tests/demo.py
@@ -6,21 +6,13 @@ def my_function(my_input):
     return my_input * 2
 
 
-<<<<<<< Updated upstream
-inputs = list(range(8))
-=======
 inputs = list(range(5_000_000))
->>>>>>> Stashed changes
 
 results = remote_parallel_map(my_function, inputs)
 
 print(f"num results: {len(results)}")
-<<<<<<< Updated upstream
-print(results)
-=======
 # print("results: (document claimed at times)")
 # print(results)
 
 # unprocessed_inputs = [i for i in inputs if i not in results]
 # print(f"{len(unprocessed_inputs)} unprocessed inputs: {unprocessed_inputs}")
->>>>>>> Stashed changes

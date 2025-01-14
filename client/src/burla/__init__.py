309d2e39f999685a72ed2cca70f88555ece1a33f
Jacob Zuliani <jkzuliani40@gmail.com>
2025-01-14 17:44:05 -0500

    better testing, indicates version on node boot (#21)

--- client/src/burla/__init__.py
+++ client/src/burla/__init__.py
@@ -17,7 +17,7 @@ else:
 _BURLA_BACKEND_URL = "https://backend.burla.dev"
 
 # needed for service to record client version
-__version__ = "0.8.17"
+__version__ = "0.8.18"
 
 from burla._auth import login
 from burla._remote_parallel_map import remote_parallel_map

da3b3f9c2e009ba3b95d2c8e782a61b07586e6a6
Jake Zuliani <jkzuliani40@gmail.com>
2025-01-28 19:21:41 -0500

    remove unused package

--- main_service/frontend/src/App.tsx
+++ main_service/frontend/src/App.tsx
@@ -1,13 +1,9 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <BrowserRouter>
@@ -16,7 +12,6 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
-  </QueryClientProvider>
 );
 
 export default App;

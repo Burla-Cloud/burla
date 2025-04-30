b875e820067fd580d2c2346329ebaab0781841e8
Jake Zuliani <jkzuliani40@gmail.com>
2025-04-30 18:10:59 -0400

    stuff

--- client/tests/demo.py
+++ client/tests/demo.py
@@ -8,11 +8,8 @@ os.environ["BURLA_API_URL"] = "http://localhost:5001"
 
 def test_function(x):
 
-    print("hi")
     sleep(0.1)
 
-    # return x
 
-
-my_inputs = list(range(1_000))
+my_inputs = list(range(1_000_000))
 remote_parallel_map(test_function, my_inputs, background=True)

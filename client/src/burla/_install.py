0893a5ee97cf6ac5ef533c6c06512bf09f4ba2cd
Jake Zuliani <jkzuliani40@gmail.com>
2025-06-11 12:27:44 -0400

    nump version, fix bug where default setting for cloudrun exceeded defauly quota

--- client/src/burla/_install.py
+++ client/src/burla/_install.py
@@ -299,7 +299,7 @@ def _install(spinner):
         f"--region=us-central1 "
         f"--service-account {SA_EMAIL} "
         f"--min-instances 1 "
-        f"--max-instances 20 "
+        f"--max-instances 5 "
         f"--memory 4Gi "
         f"--cpu 1 "
         f"--timeout 10 "

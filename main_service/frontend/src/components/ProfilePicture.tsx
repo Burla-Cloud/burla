eb9f2a81b67eb4d28507d710eea5d27a7fa58287
Jake Zuliani <jkzuliani40@gmail.com>
2025-06-25 14:01:03 -0400

    add log out button

--- main_service/frontend/src/components/ProfilePicture.tsx
+++ main_service/frontend/src/components/ProfilePicture.tsx
@@ -52,7 +52,7 @@ export default function ProfilePicture() {
             />
 
             {isOpen && (
-                <div className="absolute top-full mt-2 right-0 z-50 bg-white border border-gray-200 rounded-xl shadow-lg p-4 pt-6 w-56 text-center">
+                <div className="absolute top-full mt-2 right-0 z-50 bg-white border border-gray-200 rounded-xl shadow-lg px-4 pt-6 pb-2 w-56 text-center">
                     <img
                         src={profilePicUrl}
                         alt="User profile large"
@@ -60,6 +60,39 @@ export default function ProfilePicture() {
                     />
                     <p className="mt-2 font-semibold text-gray-800">Hi {firstName} !</p>
                     <p className="text-sm text-gray-600 mt-1">logged in as {userEmail}</p>
+                    <hr className="border-t border-gray-200 mt-6 mb-2" />
+                    <button className="flex items-center w-full text-left px-2 py-1 hover:bg-gray-100 rounded-md">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5 text-gray-600 mr-2 stroke-current"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"
+                            />
+                            <polyline
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                points="16 17 21 12 16 7"
+                            />
+                            <line
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                x1="21"
+                                y1="12"
+                                x2="9"
+                                y2="12"
+                            />
+                        </svg>
+                        <span className="text-sm text-gray-800">Log out</span>
+                    </button>
                 </div>
             )}
         </div>

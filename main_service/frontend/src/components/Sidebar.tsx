bceff55d188acdfe6cf856cc224c12605e8ceafb
Jake Zuliani <jkzuliani40@gmail.com>
2025-06-29 23:07:28 -0400

    make sidebar look better

--- main_service/frontend/src/components/Sidebar.tsx
+++ main_service/frontend/src/components/Sidebar.tsx
@@ -3,11 +3,15 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
-        <div className="w-64 min-h-screen bg-gray-100 border-r p-4 flex flex-col">
+        <div className="w-60 min-h-screen bg-gray-100 border-r p-4 flex flex-col">
             {/* Logo Container (Lowered a bit using `mt-2`) */}
             <div className="flex justify-left mt-5 mb-4">
                 <Link to="/">
-                    <img src={clusterImage} className="w-28 h-auto ml-2" />
+                    <img
+                        src={clusterImage}
+                        style={{ width: "128px", height: "auto" }}
+                        className="ml-2"
+                    />
                 </Link>
             </div>
 
@@ -15,7 +19,7 @@ const Sidebar = () => {
             <hr className="border-gray-300 my-5 w-full" />
 
             {/* Navigation Links */}
-            <nav className="space-y-1">
+            <nav className="space-y-1 text-lg font-medium">
                 <Link
                     to="/"
                     className="flex items-center space-x-1 text-gray-700 hover:text-black hover:bg-gray-200 p-2 rounded-md"

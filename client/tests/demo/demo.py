2ddf6af3341cb49711570923963cce462c5690a7
Jake Zuliani <jkzuliani40@gmail.com>
2025-07-16 12:59:27 -0400

    stuff

--- client/tests/demo/demo.py
+++ client/tests/demo/demo.py
@@ -3,7 +3,7 @@ from burla import remote_parallel_map
 
 def my_function(my_input):
 
-    print(f"I'm running input on a remote computer in the cloud!  #{my_input}")
+    print(f"I'm running on my own remote computer in the cloud!  #{my_input}")
 
 
 remote_parallel_map(my_function, list(range(1000)))

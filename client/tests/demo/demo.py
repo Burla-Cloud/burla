474bf94b6f1e2b823a8c8ff59571bbea21b03dd2
Jake Zuliani <jkzuliani40@gmail.com>
2025-07-17 12:55:02 -0400

    fix frontend and demo update

--- client/tests/demo/demo.py
+++ client/tests/demo/demo.py
@@ -1,15 +1,10 @@
+from time import sleep
 from burla import remote_parallel_map
 
 
 def my_function(my_input):
-
     # Something intense goes here!
-
-    return my_input * 2
-
-
-return_values = remote_parallel_map(my_function, list(range(10_000_000)))
-print(return_values)
+    sleep(0.1)
 
 
-pass
+remote_parallel_map(my_function, list(range(10_000_000)))

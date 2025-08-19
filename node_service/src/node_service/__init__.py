d31e1109756fbf95401d073025bc56d8ff3439ff
Jake Zuliani <jkzuliani40@gmail.com>
2025-08-19 14:26:15 -0400

    version bump

--- node_service/src/node_service/__init__.py
+++ node_service/src/node_service/__init__.py
@@ -22,7 +22,7 @@ from starlette.datastructures import UploadFile
 from google.cloud import logging, secretmanager, firestore
 from google.cloud.compute_v1 import InstancesClient
 
-__version__ = "1.1.9"
+__version__ = "1.2.0"
 CREDENTIALS, PROJECT_ID = google.auth.default()
 BURLA_BACKEND_URL = "https://backend.burla.dev"
 

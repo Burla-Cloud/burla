86e749f3c12427b6c0efe6f609b42a623eee4143
Jake Zuliani <jkzuliani40@gmail.com>
2025-08-28 13:38:19 -0400

    version bump

--- examples/basic.py
+++ examples/basic.py
@@ -1,15 +1,18 @@
 from time import sleep
 from burla import remote_parallel_map
 
-my_inputs = list(range(1000))
+my_inputs = list(range(1))
 
 
-def my_function5(my_input):
+def my_function6(my_input):
     # sleep(1)
-    # if my_input == 20:
+    # if my_input == 200:
     #     raise Exception("This is a test error")
-    print(f"Running Input #{my_input} on it's own separate computer in the cloud!")
+
+    for i in range(100):
+        print(f"Running Input #{my_input} on it's own separate computer in the cloud! {i}")
+        sleep(1)
 
 
-# remote_parallel_map(my_function2, my_inputs)
-remote_parallel_map(my_function5, my_inputs)
+# remote_parallel_map(my_function6, my_inputs)
+remote_parallel_map(my_function6, my_inputs, background=True)

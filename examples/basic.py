105ed75866dbb66af34bcb137dc2e901f1ec7eaa
Jake Zuliani <jkzuliani40@gmail.com>
2025-09-04 11:17:32 -0400

    version bump

--- examples/basic.py
+++ examples/basic.py
@@ -1,27 +1,3 @@
-# from time import sleep
-# from burla import remote_parallel_map
-
-# my_inputs = list(range(1000))
-
-
-# def my_function(my_input):
-#     sleep(1)
-#     print(f"Running Input #{my_input} on it's own separate computer in the cloud!")
-
-
-# remote_parallel_map(my_function, my_inputs)
-
-
-from burla import remote_parallel_map
-
-
-def my_function(x):
-    print(f"Running on a remote computer in the cloud! #{x}")
-
-
-remote_parallel_map(my_function, list(range(1000)))
-
-
 from burla import remote_parallel_map
 
 

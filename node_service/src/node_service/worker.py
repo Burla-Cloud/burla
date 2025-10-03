c772e967f2b29806717f4c55be7ebfec805e2f55
Jake Zuliani <jkzuliani40@gmail.com>
2025-10-03 15:07:19 -0400

    temp

--- node_service/src/node_service/worker.py
+++ node_service/src/node_service/worker.py
@@ -159,7 +159,7 @@ class Worker:
             # Restart automatically if it dies (IMPORTANT!):
             # Because it kills itself intentionally when it needs to cancel a running job.
             while true; do
-                uv run uvicorn worker_service:app --host 0.0.0.0 \
+                $python_cmd -m uvicorn worker_service:app --host 0.0.0.0 \
                     --port {WORKER_INTERNAL_PORT} --workers 1 \
                     --timeout-keep-alive 30
                 echo "HERE!"

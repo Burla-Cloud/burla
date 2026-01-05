901a191c38d2bed3da42b1b63e69a52612f12cf4
Jake Zuliani <jkzuliani40@gmail.com>
2026-01-05 14:14:08 -0500

    1.3.29

--- node_service/src/node_service/__init__.py
+++ node_service/src/node_service/__init__.py
@@ -30,7 +30,7 @@ from starlette.requests import ClientDisconnect
 from starlette.datastructures import UploadFile
 
 
-__version__ = "1.3.28"
+__version__ = "1.3.29"
 CREDENTIALS, PROJECT_ID = google.auth.default()
 BURLA_BACKEND_URL = "https://backend.burla.dev"
 

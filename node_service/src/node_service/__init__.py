6f985f88f4251f2ade3ee9168bf0c67bfcfe271a
Jacob Zuliani <jkzuliani40@gmail.com>
2026-02-16 00:57:55 -0500

    1.3.35 (#136)
    * add microsoft login
    
    * version bump

--- node_service/src/node_service/__init__.py
+++ node_service/src/node_service/__init__.py
@@ -30,7 +30,7 @@ from starlette.requests import ClientDisconnect
 from starlette.datastructures import UploadFile
 
 
-__version__ = "1.3.34"
+__version__ = "1.3.35"
 CREDENTIALS, PROJECT_ID = google.auth.default()
 BURLA_BACKEND_URL = "https://backend.burla.dev"
 

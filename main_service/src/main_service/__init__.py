48bd395616bf8b6668b779a988cf0f27764454a4
Jake Zuliani <jkzuliani40@gmail.com>
2026-03-13 14:08:10 -0400

    update lowest acceptable verison

--- main_service/src/main_service/__init__.py
+++ main_service/src/main_service/__init__.py
@@ -23,7 +23,7 @@ os.environ["GRPC_VERBOSITY"] = "ERROR"
 os.environ["GLOG_minloglevel"] = "2"
 
 CURRENT_BURLA_VERSION = "1.4.3"
-MIN_COMPATIBLE_CLIENT_VERSION = "1.4.2"
+MIN_COMPATIBLE_CLIENT_VERSION = "1.4.3"
 
 # In this mode EVERYTHING runs locally in docker containers.
 # possible modes: local-dev-mode (everything local), remote-dev-mode (only main-service local), prod

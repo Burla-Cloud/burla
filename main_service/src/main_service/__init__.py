02798b7dac92edde3d5af3ac66a498c1e924cd2c
Jake Zuliani <jkzuliani40@gmail.com>
2026-03-13 14:11:12 -0400

    version bump

--- main_service/src/main_service/__init__.py
+++ main_service/src/main_service/__init__.py
@@ -22,8 +22,8 @@ from jinja2 import Environment, FileSystemLoader
 os.environ["GRPC_VERBOSITY"] = "ERROR"
 os.environ["GLOG_minloglevel"] = "2"
 
-CURRENT_BURLA_VERSION = "1.4.3"
-MIN_COMPATIBLE_CLIENT_VERSION = "1.4.3"
+CURRENT_BURLA_VERSION = "1.4.4"
+MIN_COMPATIBLE_CLIENT_VERSION = "1.4.4"
 
 # In this mode EVERYTHING runs locally in docker containers.
 # possible modes: local-dev-mode (everything local), remote-dev-mode (only main-service local), prod

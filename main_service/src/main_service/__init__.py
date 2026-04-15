08db5fefdac3e59c0fa919c47388aa6e67f7a686
Jake Zuliani <jkzuliani40@gmail.com>
2026-04-14 22:44:54 -0400

    verion bump

--- main_service/src/main_service/__init__.py
+++ main_service/src/main_service/__init__.py
@@ -22,7 +22,7 @@ from jinja2 import Environment, FileSystemLoader
 os.environ["GRPC_VERBOSITY"] = "ERROR"
 os.environ["GLOG_minloglevel"] = "2"
 
-CURRENT_BURLA_VERSION = "1.5.3"
+CURRENT_BURLA_VERSION = "1.5.4"
 MIN_COMPATIBLE_CLIENT_VERSION = "1.5.2"
 
 # In this mode EVERYTHING runs locally in docker containers.

aa3bb1d226ad003204c08ace21376ba3da40046b
Jake Zuliani <jkzuliani40@gmail.com>
2026-08-15 13:03:41 -0400

    Release 1.6.6

--- node_service/src/node_service/__init__.py
+++ node_service/src/node_service/__init__.py
@@ -19,7 +19,7 @@ from fastapi.responses import Response
 from starlette.datastructures import UploadFile
 from starlette.requests import ClientDisconnect
 
-__version__ = "1.6.5"
+__version__ = "1.6.6"
 PROJECT_ID = os.environ["PROJECT_ID"]
 BURLA_BACKEND_URL = os.environ.get(
     "BURLA_BACKEND_URL", "https://backend.burla.dev"

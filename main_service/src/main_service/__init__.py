7830d0d6cf795a1aafd3c4e61d27c69921ec1198
Jake Zuliani <jkzuliani40@gmail.com>
2026-08-18 16:24:40 -0400

    Release 1.7.0

--- main_service/src/main_service/__init__.py
+++ main_service/src/main_service/__init__.py
@@ -28,8 +28,8 @@ from jinja2 import Environment, FileSystemLoader
 from starlette.datastructures import UploadFile
 from starlette.middleware.sessions import SessionMiddleware
 
-CURRENT_BURLA_VERSION = "1.6.6"
-MIN_COMPATIBLE_CLIENT_VERSION = "1.6.6"
+CURRENT_BURLA_VERSION = "1.7.0"
+MIN_COMPATIBLE_CLIENT_VERSION = "1.7.0"
 NODE_SOURCE_REF = os.environ.get("BURLA_NODE_SOURCE_REF", CURRENT_BURLA_VERSION)
 
 # In this mode EVERYTHING runs locally in docker containers.
--- node_service/src/node_service/__init__.py
+++ node_service/src/node_service/__init__.py
@@ -18,7 +18,7 @@ from fastapi.responses import Response
 from starlette.datastructures import UploadFile
 from starlette.requests import ClientDisconnect
 
-__version__ = "1.6.6"
+__version__ = "1.7.0"
 PROJECT_ID = os.environ["PROJECT_ID"]
 BURLA_BACKEND_URL = os.environ.get(
     "BURLA_BACKEND_URL", "https://backend.burla.dev"
